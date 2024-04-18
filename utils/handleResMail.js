dda6c04c6678e82e01fc91ede150116d559238c3
MansourMahmoud <mansourmahmoud77a@gmail.com>
2024-04-18 15:09:25 +0200

    second commit

--- utils/handleResMail.js
+++ utils/handleResMail.js
@@ -8,7 +8,7 @@ module.exports = (status, course, branch, token) => {
     <div style="text-align: right;">
         <p>السلام عليكم، لقد تم قبولكم في أكادمية تفوّق كأستاذ للفرع ${branch} لدورة ${course}.</p>
         <p>الرجاء إكمال عملية التسجيل حتى تتمكن من الدخول إلى الموقع.</p>
-        <p>لإكمال عملية التسجيل وضبط كلمة السر الخاصة بكم الرجاء الضغط <a href="${process.env.BASE_URL_FRONTEND}/teachers/set-password-teacher/t=${token}" target="_blank">هنا</a>.</p>
+        <p>لإكمال عملية التسجيل وضبط كلمة السر الخاصة بكم الرجاء الضغط <a href="${process.env.BASE_URL_FRONTEND}/teachers/set-password-teacher/?t=${token}" target="_blank">هنا</a>.</p>
     </div>
     `;
   } else if (status === refused) {

3ddc9c9a5dc21e9549879076cbdc8099941138f1
MansourMahmoud <mansourmahmoud77a@gmail.com>
2024-04-21 13:03:30 +0200

    cors change

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const cors = require("cors");
 const { ERROR } = require("./utils/httpStatusText");
 require("dotenv").config();
 require("./DB/DBConnect");
-const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT;
 
-app.use(cors({ origin: "*", methods: "*" }));
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL ?? "http://localhost:3000",
+    optionsSuccessStatus: 200,
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 

900678b9bb5d3ba62e6d53548a7364c6d8f76418
MansourMahmoud <mansourmahmoud77a@gmail.com>
2024-04-21 12:59:52 +0200

    cors change

--- index.js
+++ index.js
@@ -6,16 +6,10 @@ require("./DB/DBConnect");
 const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT;
+
+app.use(cors({ origin: "*", methods: "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use(
-  cors({
-    credentials: true,
-    origin: process.env.FRONTEND_URL ?? "http://localhost:3000",
-    optionsSuccessStatus: 200,
-  })
-);
 
 // import Router
 const teachersRouter = require("./routes/teacher.route");
